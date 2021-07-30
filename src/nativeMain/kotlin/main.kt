6f2f536f8beeb206ab2e350a952d81a31ca02d79
Tetramad <cbwjd777@gmail.com>
2021-07-30 19:44:48 +0900

    feat: multiple file manipulation
    Signed-off-by: Tetramad <cbwjd777@gmail.com>

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,14 +1,32 @@
 import com.github.tetramad.kotlinnative.*
 import com.github.tetramad.kwc.*
+import kotlin.math.max
 
 /**
- *  last modified 2021.07.25 by Tetramad
+ *  last modified 2021.07.30 by Tetramad
  */
-fun main(/*args: Array<String>*/) {
-    val path = "input.txt"
-    val file = fileOpen(path, modes = "r")
-    val content = file.map(::fileContent).getOrThrow()
-    file.onSuccess(::fileClose)
+fun main(args: Array<String>) {
+    val programName = "kwc"
+    val results = args.map {
+        val file = fileOpen(it, modes = "r")
+        val content = file.map(::fileContent)
+        file.onSuccess(::fileClose)
+        content
+    }.map {
+        it.map { content ->
+            var linesString = lines(content).toString()
+            linesString = linesString.padStart(max(0, 3 - linesString.length), ' ')
+            var wordsString = words(content).toString()
+            wordsString = wordsString.padStart(max(0, 3 - wordsString.length), ' ')
+            var bytesString = bytes(content).toString()
+            bytesString = bytesString.padStart(max(0, 3 - bytesString.length), ' ')
+            "$linesString $wordsString $bytesString" }
+    }
 
-    print("${lines(content)} ${words(content)} ${bytes(content)}")
+    args.zip(results).forEach { (fileName, result) ->
+        println(result.fold(
+            { content -> "${content} ${fileName}" },
+            { exception -> "${programName}: ${fileName}: ${exception.message ?: "Unknown Error"}" }
+        ))
+    }
 }
