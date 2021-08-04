3c283fcd64f46fbe369bc497881cd261b72c0373
Tetramad <cbwjd777@gmail.com>
2021-08-04 22:22:38 +0900

    deps: enable endorsed libraries
    Signed-off-by: Tetramad <cbwjd777@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,9 @@ kotlin {
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
         hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
+        hostOs == "Linux" -> linuxX64("native") {
+            compilations["main"].enableEndorsedLibs = true
+        }
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
